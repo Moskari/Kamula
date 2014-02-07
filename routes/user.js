a7af7c1746fc3a3be53cdcc380d6c5531b1e5353
Moskari <moskari11@gmail.com>
2014-02-07 11:02:04 -0500

    Link now takes to a page that has couple of fake user names

--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,6 @@
  */
 
 exports.list = function(req, res){
-  res.send("respond with a resource");
+  //res.send("respond with a resource");
+  res.render('users', {'title':'Users', 'users':['monni','isomonni']});
 };
