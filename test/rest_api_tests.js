52a8691be59c4e1e9998fd548d88b5425a5fe0c9
Moskari <Moskari11@gmail.com>
2014-05-26 21:13:41 +0300

    Added one somewhat working test case for mocha. It uses supertest and Should modules also.

---
+++ test/rest_api_tests.js
@@ -0,0 +1,135 @@
+var assert = require("assert")
+
+var User = require('../models/user').User;
+var Message = require('../models/message').Message;
+var mongoose = require('mongoose');
+
+/*
+describe('Connection', function(){
+  var uri = 'mongodb://localhost/kamula';
+  mongoose.connect(uri);
+  var db = new Connection
+    , tobi = new User('tobi')
+    , loki = new User('loki')
+    , jane = new User('jane');
+
+  beforeEach(function(done){
+    mongoose.connection.db.dropDatabase();
+    db.clear(function(err){
+      if (err) return done(err);
+      db.save([tobi, loki, jane], done);
+    });
+  })
+
+  describe('#find()', function(){
+    it('respond with matching records', function(done){
+      db.find({ type: 'User' }, function(err, res){
+        if (err) return done(err);
+        res.should.have.length(3);
+        done();
+      })
+    })
+  })
+})
+*/
+
+
+var should = require('should');
+var assert = require('assert');
+var request = require('supertest');
+var mongoose = require('mongoose');
+ 
+describe('Requesting api', function() {
+	var url = 'localhost:3000';
+	var uri = 'mongodb://localhost/kamula';
+	
+	// within before() you can run all the operations that are needed to setup your tests. In this case
+	// I want to create a connection with the database, and when I'm done, I call done().
+	before(function(done) {
+		// In our tests we use the test db
+		
+		mongoose.connect(uri);	
+		mongoose.connection.collections['users'].drop( function(err) { });
+		//mongoose.connection.db.dropDatabase();
+		//User.remove({}, function(err) {
+		//	console.log('User collection removed');
+		//	
+		//});
+		done();
+	});
+	// use describe to give a title to your test suite, in this case the tile is "Account"
+	// and then specify a function in which we are going to declare all the tests
+	// we want to run. Each test starts with the function it() and as a first argument
+	// we have to provide a meaningful title for it, whereas as the second argument we
+	// specify a function that takes a single parameter, "done", that we will use
+	// to specify when our test is completed, and that's what makes easy
+	// to perform async test!
+	describe('User', function() {
+		it('should return 201 when registering an user', function(done) {
+			var profile = {
+				"user": "jeejee",
+				"name": "Matti Nykänen",
+				"email": "jeejee@jj",
+				"password": "jeesukset"
+			}
+			// once we have specified the info we want to send to the server via POST verb,
+			// we need to actually perform the action on the resource, in this case we want to
+			// POST on /api/profiles and we want to send some info
+			// We do this using the request object, requiring supertest!
+			request(url)
+			.post('/api/users/ ')
+			.send(profile)
+			// end handles the response
+			.end(function(err, res) {
+				if (err) {
+					throw err;
+				}
+				// this is should.js syntax, very clear
+				res.should.have.status(201);
+				/*User.findOne({user : "jeejee1232"}, function(err, user) {
+					if (!user || err) {
+						false.should.be.true; // How else could you test it?
+					} else {
+						user.should.not.be.empty;
+						user.should.not.be.NaN;
+						user.password.should.equal("jeesukset");
+						user.email.should.equal("jeejee@jj");
+						user.name.should.equal("Matti Nykänen");
+						
+					}
+					done();
+				});*/
+				done();
+			});
+		});
+		/*
+		it('should correctly update an existing account', function(done){
+			var body = {
+				firstName: 'JP',
+				lastName: 'Berd'
+			};
+			request(url)
+				.put('/api/profiles/vgheri')
+				.send(body)
+				.expect('Content-Type', /json/)
+				.expect(200) //Status code
+				.end(function(err,res) {
+					if (err) {
+						throw err;
+					}
+					// Should.js fluent syntax applied
+					res.body.should.have.property('_id');
+					res.body.firstName.should.equal('JP');
+					res.body.lastName.should.equal('Berd');
+					res.body.creationDate.should.not.equal(null);
+					done();
+				});
+		}); */
+	});
+	
+	
+	after(function(done) {
+		mongoose.connection.collections['users'].drop( function(err) { });
+		done();
+	});
+});
